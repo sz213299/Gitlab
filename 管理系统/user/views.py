b54a44260529ed839140a44a85784d75c5cf773d
sz213299 <2132998280@qq.com>
2024-09-24 19:23:44 +0800

    第二次提交

--- 管理系统/user/views.py
+++ 管理系统/user/views.py
@@ -1,53 +1,97 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from rest_framework_jwt.settings import api_settings
 
+from menu.models import SysMenu, SysMenuSerializer
+from role.models import SysRole
 from user.models import SysUser, SysUserSerializer
-from django.http import JsonResponse
+
+
+class LoginView(View):
+
+    def buildTreeMenu(self, sysMenuList):
+        resultMenuList: list[SysMenu] = list()
+        for menu in sysMenuList:
+            # 寻找子节点
+            for e in sysMenuList:
+                if e.parent_id == menu.id:
+                    if not hasattr(menu, "children"):
+                        menu.children = list()
+                    menu.children.append(e)
+            # 判断父节点，添加到集合
+            if menu.parent_id == 0:
+                resultMenuList.append(menu)
+        return resultMenuList
+
+    def post(self, request):
+        username = request.GET.get("username")
+        password = request.GET.get("password")
+        try:
+            user = SysUser.objects.get(username=username, password=password)
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+            # 将用户对象传递进去，获取到该对象的属性值
+            payload = jwt_payload_handler(user)
+            # 将属性值编码成jwt格式的字符串
+            token = jwt_encode_handler(payload)
+
+            roleList = SysRole.objects.raw(
+                "SELECT id ,NAME FROM sys_role WHERE id IN (SELECT role_id FROM sys_user_role WHERE user_id=" + str(
+                    user.id) + ")")
+            print(roleList)
+            menuSet: set[SysMenu] = set()
+            for row in roleList:
+                print(row.id, row.name)
+                menuList = SysMenu.objects.raw(
+                    "SELECT * FROM sys_menu WHERE id IN (SELECT menu_id FROM sys_role_menu WHERE role_id=" + str(
+                        row.id) + ")")
+                for row2 in menuList:
+                    print(row2.id, row2.name)
+                    menuSet.add(row2)
+            print(menuSet)
+            menuList: list[SysMenu] = list(menuSet)  # set转list
+            sorted_menuList = sorted(menuList)  # 根据order_num排序
+            print(sorted_menuList)
+            # 构造菜单树
+            sysMenuList: list[SysMenu] = self.buildTreeMenu(sorted_menuList)
+            print(sysMenuList)
+            serializerMenuList = list()
+            for sysMenu in sysMenuList:
+                serializerMenuList.append(SysMenuSerializer(sysMenu).data)
+
+        except Exception as e:
+            print(e)
+            return JsonResponse({'code': 500, 'info': '用户名或者密码错误！'})
+        return JsonResponse({'code': 200, 'token': token, 'user': SysUserSerializer(user).data, 'info': '登录成功',
+                             'menuList': serializerMenuList})
 
 
 # Create your views here.
-class LoginView(View):
-    def post(self, request):
-        username = request.GET.get('username')
-        password = request.GET.get('password')
-        try:
-            user = SysUser.objects.get(username=username, password=password)
-            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER  # 载荷
-            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER  # 解码
-            # 将用户对象传进去，获取到搞对象的属性
-            paylod = jwt_payload_handler(user)
-            # 将属性值编码成jwt格式的字符串
-            token = jwt_encode_handler(paylod)
-        except Exception as e:
-            print(e)
-            return JsonResponse({'code': 500, 'info': '用户名或密码错误'})
-        # 传入实例化对象，有一个属性data，直接返回实例化对象
-        return JsonResponse({'code': 200, 'token': token, 'user': SysUserSerializer(user).data, 'info': '登录成功', })
-
-
 class TestView(View):
+
     def get(self, request):
-        # 获取token
         token = request.META.get('HTTP_AUTHORIZATION')
-        if token is not None and token != '':
-            # json  序列还
-            userList_obj = SysUser.objects.all()  # 所有用户信息
-            user_dict = userList_obj.values()  # 转换为字典
-            UserList = list(user_dict)  # 把外层容器转换成列表
-            print(UserList)
-            return JsonResponse({'code': 200, 'info': '测试', 'data': UserList})
+        if token != None and token != '':
+            userList_obj = SysUser.objects.all()
+            print(userList_obj, type(userList_obj))
+            userList_dict = userList_obj.values()  # 转存字典
+            print(userList_dict, type(userList_dict))
+            userList = list(userList_dict)  # 把外层的容器转存List
+            print(userList, type(userList))
+            return JsonResponse({'code': 200, 'info': '测试！', 'data': userList})
         else:
-            return JsonResponse({'code': 401, 'info': '没有访问权限', })
+            return JsonResponse({'code': 401, 'info': '没有访问权限！'})
 
 
 class JwtTestView(View):
+
     def get(self, request):
-        user = SysUser.objects.get(username='sz', password='123456')
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER  # 载荷
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER  # 解码
-        # 将用户对象传进去，获取到搞对象的属性
-        paylod = jwt_payload_handler(user)
+        user = SysUser.objects.get(username='python222', password='123456')
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        # 将用户对象传递进去，获取到该对象的属性值
+        payload = jwt_payload_handler(user)
         # 将属性值编码成jwt格式的字符串
-        token = jwt_encode_handler(paylod)
+        token = jwt_encode_handler(payload)
         return JsonResponse({'code': 200, 'token': token})
